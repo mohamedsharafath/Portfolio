0b88e1352ae63dde46c8a880ed2d65322dedaa76
mohamedsharafath <sharafath0sms@gmail.com>
2024-02-07 16:32:28 +0530

    commit 2

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header/Header';
+import Home from './components/home/home';
+function App() {
+  return (
+    <>
+    <Header/>
+    <main className='main'>
+      <Home/>
+    </main>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home_social">
+        <a href="" className="home_social-icon" target="_blank">
+            <i className='uil uil-instagram'></i>
+        </a>
+        <a href="" className="home_social-icon" target="_blank">
+            <i className='uil uil-facebook'></i>
+
+        </a>
+        <a href="" className="home_social-icon" target="_blank">
+            <i className='uil uil-github-alt'></i>
+
+        </a>
+    </div>
+  )
+}
+
+export default Social
